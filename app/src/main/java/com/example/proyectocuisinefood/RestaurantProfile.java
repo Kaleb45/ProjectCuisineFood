31b52621115706db994a5a71384d0437bf549b2b
Kaleb45 <a20300663@ceti.mx>
2024-04-14 13:58:17 -0600

    Restaurant Profile 1.0

---
+++ app/src/main/java/com/example/proyectocuisinefood/RestaurantProfile.java
@@ -0,0 +1,14 @@
+package com.example.proyectocuisinefood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_profile);
+    }
+}
