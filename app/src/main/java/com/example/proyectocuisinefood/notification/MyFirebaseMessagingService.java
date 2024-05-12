9d21db6c98f8512ae8e053e4d5a2474acb4699c8
Kaleb45 <a20300663@ceti.mx>
2024-05-11 22:01:34 -0600

    Update finals 8.4, Notifications

--- app/src/main/java/com/example/proyectocuisinefood/notification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/proyectocuisinefood/notification/MyFirebaseMessagingService.java
@@ -39,6 +39,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
 import com.google.gson.JsonIOException;
 
 import org.json.JSONException;
